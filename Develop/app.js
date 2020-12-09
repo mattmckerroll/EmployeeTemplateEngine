5a9f49ef0aee9e06fb1e4d8e220266b6b00296f3
mattmck <matthew.mckerroll@gmail.com>
2020-12-08 23:42:45 -0500

    finsihed up logic for app.js

--- Develop/app.js
+++ Develop/app.js
@@ -9,9 +9,123 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+let employees = [];
 
 
+function init(){
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: `What is your manager's name?`
+      },
+      {
+        type: "input",
+        name: "id",
+        message: `What is your manager's ID?`
+      },
+      {
+        type: "input",
+        name: "email",
+        message: `What is your manager's email?`
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: `What is your manager's office number?`
+      }
+    ])
+  }
+  function buildTeam(){
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "role",
+        message:"What type of team member would you like to add?",
+        choices: ["Engineer","Intern","I don't want to add any more team members"]
+      }
+    ]).then((answer)=> {
+      if (answer.role === "Engineer"){
+        return inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: `What is your engineer's name?`
+          },
+          {
+            type: "input",
+            name: "id",
+            message: `What is your engineer's ID?`
+          },
+          {
+            type: "input",
+            name: "email",
+            message: `What is your engineer's email?`
+          },
+          {
+            type: "input",
+            name: "github",
+            message: `What is your engineer's GitHub username?`
+          }
+        ]).then((answers)=>{
+          let engineer = new Engineer(answers.name, answers.id, answers.email,answers.github);
+          employees.push(engineer);
+          buildTeam();
+        })
+      }
+      if (answer.role === "Intern"){
+        return inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: `What is your intern's name?`
+          },
+          {
+            type: "input",
+            name: "id",
+            message: `What is your intern's ID?`
+          },
+          {
+            type: "input",
+            name: "email",
+            message: `What is your intern's email?`
+          },
+          {
+            type: "input",
+            name: "school",
+            message: `What is your intern's school?`
+          }
+        ]).then((answers)=>{
+          let intern = new Intern(answers.name, answers.id, answers.email,answers.school);
+          employees.push(intern);
+          buildTeam();
+        })
+      }
+      
 
+      printHTML(employees);
+      
+    });
+  }
+  function printHTML(data){
+    fs.writeFile('../output/team.html' ,render(data), (err) => {
+      if(err) {
+        throw err;
+      };
+      console.log("Your team has been constructed!");
+    });
+ 
+    };
+  
+
+  init()
+  .then((answers)=>{
+    const manager = new Manager(answers.name, answers.id, answers.email,answers.officeNumber);
+    employees.push(manager);
+    buildTeam();
+    
+
+  });
 
 
 // Write code to use inquirer to gather information about the development team members,
